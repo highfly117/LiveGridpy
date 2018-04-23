fae84b0565e659b39358f3e07183ca5bd53f70ce
highfly117 <32913685+highfly117@users.noreply.github.com>
2018-04-23 22:56:57 +0100

    Add files via upload

--- LiveGrid.py
+++ LiveGrid.py
@@ -2,29 +2,46 @@
 # -*- coding: utf-8 -*-
 
 import csv
-import urllib2
+import urllib.request
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.plotly as py
+import plotly as py
+
+
+py.tools.set_credentials_file(username='highfly117', api_key='2pQvxqeQNhSf07JUc4Nh')
 
 
 url = 'https://api.bmreports.com/BMRS/FUELINST/v1?APIKey=66ky5jo5p5w0vbd&ServiceType=CSV'
 url2 = 'https://api.bmreports.com/BMRS/FUELINSTHHCUR/v1?APIKey=66ky5jo5p5w0vbd&ServiceType=CSV'
-response = urllib2.urlopen(url2)
-cr = csv.reader(response)
+response = urllib.request.urlopen(url2)
 
+arr = np.genfromtxt(response,delimiter=",", skip_header=1, skip_footer=2,dtype=str, usecols=(1,2))
 
-arr = np.genfromtxt(response,delimiter=",", skip_header=1, skip_footer=2,dtype=None)
+n = 0
+fueltype = []
+gigawatt = []
+m = 0
+x = []
+y = []
 
+while n < len(arr):
+    fueltype.append(arr[m][0])
+    gigawatt.append(int(arr[m][1]))
+    n = n + 1
+    m = m + 1
 
+y_pos = np.arange(len(fueltype))
 
-y = arr[0][2]
-N = 12000
-x = range(N)
-width = 1/1.5
-plt.bar(x, y, width, color="blue")
+data = [py.graph_objs.Bar(
+    x=fueltype,
+    y=gigawatt
+    )]
 
+plt.bar(y_pos, gigawatt, align='center', alpha=0.5)
+plt.xticks(y_pos, fueltype)
+plt.ylabel('Gigawatts')
+plt.title('fueltype')
 
-fig = plt.gcf()
-plot_url = py.plot_mpl(fig, filename='mpl-basic-bar')
+plt.show()
+
 
